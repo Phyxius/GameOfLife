07c00d77497ba883f15e07ad4647df186c545df5
Shea <env.laser@gmail.com>
2015-08-31 16:07:12 -0600

    Scroll bars work

--- src/GameDrawerPanel.java
+++ src/GameDrawerPanel.java
@@ -1,6 +1,8 @@
 import javax.swing.*;
 import javax.swing.event.MouseInputAdapter;
 import java.awt.*;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseWheelEvent;
 
@@ -14,12 +16,23 @@ public class GameDrawerPanel extends JPanel
   public static final int MIN_GRID_SIZE = 5;
   private int pixelSize = 20, pixelOffsetX = 0, pixelOffsetY = 0;
   private final boolean[][] gameState = new boolean[GAME_SIZE + 2][GAME_SIZE + 2]; //account for border cells
-  public GameDrawerPanel()
+  private final JScrollBar horizontalScrollBar, verticalScrollBar;
+  public GameDrawerPanel(JScrollBar horizontalScrollBar, JScrollBar verticalScrollBar)
   {
     MouseHandler m = new MouseHandler();
     addMouseListener(m);
     addMouseMotionListener(m);
     addMouseWheelListener(m);
+    addComponentListener(new ResizeHandler());
+    this.horizontalScrollBar = horizontalScrollBar;
+    this.verticalScrollBar = verticalScrollBar;
+    updateScrollBars();
+  }
+
+  private void updateScrollBars()
+  {
+    horizontalScrollBar.setValues(pixelOffsetX, getVisibleGridWidth(), 0, GAME_SIZE - getVisibleGridWidth());
+    verticalScrollBar.setValues(pixelOffsetY, getVisibleGridHeight(), 0, GAME_SIZE - getVisibleGridHeight());
   }
 
   @Override
@@ -45,9 +58,9 @@ public class GameDrawerPanel extends JPanel
   {
     graphics.clearRect(0, 0, getWidth(), getHeight());
     graphics.setColor(Color.black);
-    for (int i = -1; i <= getWidth() / pixelSize + 1; i++)
+    for (int i = -1; i <= getVisibleGridWidth() + 1; i++)
     {
-      for (int j = -1; j <= getHeight() / pixelSize + 1; j++)
+      for (int j = -1; j <= getVisibleGridHeight() + 1; j++)
       {
         int curGridX = i + pixelOffsetX / pixelSize;
         int curGridY = j + pixelOffsetY / pixelSize;
@@ -64,27 +77,60 @@ public class GameDrawerPanel extends JPanel
     if (pixelSize >= MIN_GRID_SIZE)
     {
       graphics.setColor(Color.gray);
-      for (int i = 0; i <= getWidth() / pixelSize; i++)
+      for (int i = 0; i <= getVisibleGridWidth() + 1; i++)
       {
         int pixelX = i * pixelSize - pixelOffsetX % pixelSize;
         graphics.drawLine(pixelX, 0, pixelX, getHeight());
       }
-      for (int i = 0; i <= getHeight() / pixelSize; i ++)
+      for (int i = 0; i <= getVisibleGridHeight() + 1; i ++)
       {
         int pixelY = i * pixelSize - pixelOffsetY % pixelSize;
         graphics.drawLine(0, pixelY, getWidth(), pixelY);
       }
     }
   }
+
+  private int getVisibleGridHeight()
+  {
+    return getHeight() / pixelSize;
+  }
+
+  private int getVisibleGridWidth()
+  {
+    return getWidth() / pixelSize;
+  }
+
   public Dimension getScreenGridSize()
   {
-    return new Dimension(getWidth() / pixelSize, getHeight() / pixelSize);
+    return new Dimension(getVisibleGridWidth(), getVisibleGridHeight());
   }
-  public void scroll(int xAmount, int yAmount)
+  public void scrollRelative(int xAmount, int yAmount)
   {
     pixelOffsetX = Util.clampInteger(pixelOffsetX + xAmount, 0, GAME_SIZE - getScreenGridSize().width);
     pixelOffsetY = Util.clampInteger(pixelOffsetY + yAmount, 0, GAME_SIZE - getScreenGridSize().height);
+    updateScrollBars();
     repaint();
+  }
+
+  public void scrollXAbsolute(int x)
+  {
+    pixelOffsetX = x;
+    repaint();
+  }
+
+  public void scrollYAbsolute(int y)
+  {
+    pixelOffsetY = y;
+    repaint();
+  }
+
+  private class ResizeHandler extends ComponentAdapter
+  {
+    @Override
+    public void componentResized(ComponentEvent componentEvent)
+    {
+      updateScrollBars();
+    }
   }
 
   private class MouseHandler extends MouseInputAdapter
@@ -106,13 +152,14 @@ public class GameDrawerPanel extends JPanel
     public void mouseWheelMoved(MouseWheelEvent e)
     {
       pixelSize = Util.clampInteger(pixelSize + e.getWheelRotation(), 1, 40);
+      updateScrollBars();
       repaint();
     }
 
     @Override
     public void mouseDragged(MouseEvent mouseEvent)
     {
-      scroll(currentPoint.x - mouseEvent.getX(), currentPoint.y - mouseEvent.getY());
+      scrollRelative(currentPoint.x - mouseEvent.getX(), currentPoint.y - mouseEvent.getY());
       currentPoint = mouseEvent.getPoint();
     }
   }
--- src/GameOfLifePanel.java
+++ src/GameOfLifePanel.java
@@ -24,21 +24,23 @@ public class GameOfLifePanel extends JPanel
     c.gridwidth = 1;
     c.gridheight = 1;
     c.fill = GridBagConstraints.BOTH;
-    gameDrawerPanel = new GameDrawerPanel();
+    verticalScrollBar = new JScrollBar(Adjustable.VERTICAL);
+    horizontalScrollBar = new JScrollBar(Adjustable.HORIZONTAL);
+    gameDrawerPanel = new GameDrawerPanel(horizontalScrollBar, verticalScrollBar);
+    horizontalScrollBar.addAdjustmentListener(e -> gameDrawerPanel.scrollXAbsolute(e.getValue()));
+    verticalScrollBar.addAdjustmentListener(e -> gameDrawerPanel.scrollYAbsolute(e.getValue()));
     add(gameDrawerPanel, c);
     c.weighty = 0;
     c.weightx = 0;
     c.gridx = 1;
     c.gridwidth = GridBagConstraints.RELATIVE;
     c.fill = GridBagConstraints.VERTICAL;
-    verticalScrollBar = new JScrollBar(Adjustable.VERTICAL);
     add(verticalScrollBar, c);
     c.gridx = 0;
     c.gridy = 1;
     c.gridwidth = 1;
     c.gridheight = GridBagConstraints.RELATIVE;
     c.fill = GridBagConstraints.HORIZONTAL;
-    horizontalScrollBar = new JScrollBar(Adjustable.HORIZONTAL);
     add(horizontalScrollBar, c);
   }
 
